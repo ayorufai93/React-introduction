5df7054b5f4c5154e1473b8de0ce2cbe69e97a4e
ayorufai93 <ayorufai93@gmail.com>
2026-01-29 12:04:07 +0100

    new comitted files

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,15 +1,16 @@
 function profile() {
-    const age = 21;
-    const location = "Nigeria"
-    const status = "Frontend Developer in Training "
+  const age = 21;
+  const location = "Nigeria";
+  const status = "Frontend Developer in Training ";
 
-    return (
-        <div className="profile">
-            <p>Age:{age}</p>
-            <p>Location:{location}</p>
-            <p>Status:{status}</p>
-        </div>    
-    );
+  return (
+    <div className="profile">
+      <p>Age:{age}</p>
+      <p>Location:{location}</p>
+      <p>Status:{status}</p>
+    </div>
+  );
 }
 
 export default profile;
+console.log();
